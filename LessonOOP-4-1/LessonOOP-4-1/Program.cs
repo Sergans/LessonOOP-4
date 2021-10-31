64d061593136ec0b0b6c3b2deb6ebee711c0153e
Сергей <gan7er@yandex.ru>
2021-10-31 12:02:26 +0300

    class

--- LessonOOP-4-1/LessonOOP-4-1/Program.cs
+++ LessonOOP-4-1/LessonOOP-4-1/Program.cs
@@ -2,6 +2,14 @@
 
 namespace LessonOOP_4_1
 {
+    public class Building
+    {
+        public int Nomber {get; set;}
+        public int Floor {get; set;}
+        public int Entrance {get; set;}
+        public int Apartment {get; set;}
+        public float Height { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
