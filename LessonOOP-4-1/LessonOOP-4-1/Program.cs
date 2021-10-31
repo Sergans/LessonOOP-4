d02e88afbd7715a6f23c9a48b76eec15c10ea36d
Сергей <gan7er@yandex.ru>
2021-10-31 12:36:32 +0300

    add metod print

--- LessonOOP-4-1/LessonOOP-4-1/Program.cs
+++ LessonOOP-4-1/LessonOOP-4-1/Program.cs
@@ -4,17 +4,40 @@ namespace LessonOOP_4_1
 {
     public class Building
     {
-        public int Nomber {get; set;}
-        public int Floor {get; set;}
-        public int Entrance {get; set;}
-        public int Apartment {get; set;}
-        public float Height { get; set; }
+        private int Nomber;
+        private int Floor;
+        private int Entrance;
+        private int Apartment;
+        private float Height; 
+        private static int unicNomber; 
+        public Building(int floor,int entrance,int apartment,float height)
+        {
+            this.Floor = floor;
+            this.Entrance = entrance;
+            this.Apartment = apartment;
+            this.Height = height;
+            this.Nomber = ++unicNomber;
+
+        }
+        public void Print()
+        {
+            Console.WriteLine(
+            $"Номер здания- {this.Nomber}\r\n" +
+            $"Этажность- {this.Floor}\r\n" +
+            $"Количество подъездов- {this.Entrance}\r\n" +
+            $"Количество квартир- {this.Apartment}\r\n" +
+            $"Высота здания- {this.Height} метров");
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
-           
+            Building building1 = new Building(5, 3, 50, 10);
+            Building building2 = new Building(5, 3, 50, 10);
+            building1.Print();
+            Console.WriteLine();
+            building2.Print();
         }
     }
 }
